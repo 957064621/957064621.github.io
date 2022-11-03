cd52225ff29206894a60a8fbc8103413892b68fd
957064621 <957064621@qq.com>
2022-11-03 11:09:37 +0800

    end

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 let world;
 
 // create a variable to hold our marker
-let marker_zb, marker_hiro;
+let marker_zb, marker_hiro, marker_01, marker_02, marker_03, marker_04;
 
 let a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_10, a_11, a_12, a_13, a_14;
 
@@ -36,7 +36,15 @@ function setup() {
   //marker
   marker_zb = world.getMarker('zb');
   marker_hiro = world.getMarker('hiro');
+
+  marker_01 = world.getMarker('01');
+  marker_02 = world.getMarker('02');
+  marker_03 = world.getMarker('03');
+  marker_04 = world.getMarker('04');
   // marker_zb.add(cube);
+
+
+  //冬日温暖
 
     a_1 = loadImage("assest/a/1.png");
 
@@ -80,7 +88,45 @@ function setup() {
 
     a_14 = loadImage("assest/a/14.png");
 
+    //折荷
 
+    b_1 = loadImage("assest/b/1.png");
+
+    b_2 = createVideo(
+    'assest/b/2.webm',
+    vidLoad);
+
+    b_3 = createVideo(
+    'assest/b/3.webm',
+    vidLoad);
+
+    b_4 = createVideo(
+    'assest/b/4.webm',
+    vidLoad);
+
+    b_5 = loadImage("assest/b/5.png");
+
+    //两小
+
+    c_1 = loadImage("assest/c/1.png");
+
+    c_2 = createVideo(
+    'assest/c/2.webm',
+    vidLoad);
+
+    c_3 = createVideo(
+    'assest/c/3.webm',
+    vidLoad);
+
+    c_4 = createVideo(
+    'assest/c/4.webm',
+    vidLoad);
+
+    c_5 = createVideo(
+    'assest/c/5.webm',
+    vidLoad);
+
+    //逃避与追求
 
     d_1 = loadImage("assest/d/1.png");
 
@@ -117,13 +163,13 @@ function draw(){
 // if(read == true){
 //   background(255);
 // }else{
-    if(marker_hiro.isVisible()==true){
+    if(marker_01.isVisible()==true){
     let mk_pos = new THREE.Vector3();
-    mk_pos.setFromMatrixPosition(marker_hiro.tag.object3D.matrixWorld);
+    mk_pos.setFromMatrixPosition(marker_01.tag.object3D.matrixWorld);
     let x = mk_pos.x * width ;
     let y = mk_pos.y * -height;
     let z = mk_pos.z * width/2;
-    let mk_rot = marker_hiro.tag.object3D.rotation;
+    let mk_rot = marker_01.tag.object3D.rotation;
 
     translate(x, y, z);
     rotateX(-mk_rot.x);
@@ -199,14 +245,93 @@ function draw(){
     rect(-325, -410, 650, 820);
   }
 
-
-    if(marker_zb.isVisible()==true){
+  if(marker_02.isVisible()==true){
     let mk_pos = new THREE.Vector3();
-    mk_pos.setFromMatrixPosition(marker_zb.tag.object3D.matrixWorld);
+    mk_pos.setFromMatrixPosition(marker_02.tag.object3D.matrixWorld);
     let x = mk_pos.x * width ;
     let y = mk_pos.y * -height;
     let z = mk_pos.z * width/2;
-    let mk_rot = marker_zb.tag.object3D.rotation;
+    let mk_rot = marker_02.tag.object3D.rotation;
+
+    translate(x, y, z);
+    rotateX(-mk_rot.x);
+    rotateY(mk_rot.y);
+    rotateZ(-mk_rot.z);
+    rotateX(PI/2);
+
+    texture(b_5);
+    noStroke();
+    rect(-320, -410, 640, 820);
+
+    translate(0, 0, 50);
+    texture(b_4);
+    noStroke();
+    rect(-320, -410, 640, 820);
+
+    translate(0, 0, 50);
+    texture(b_3);
+    noStroke();
+    rect(-320, -410, 640, 820);
+
+    translate(0, 0, 50);
+    texture(b_2);
+    noStroke();
+    rect(-320, -410, 640, 820);
+
+    translate(0, 0, 50);
+    texture(b_1);
+    noStroke();
+    rect(-320, -410, 640, 820);
+  }
+
+  if(marker_03.isVisible()==true){
+    let mk_pos = new THREE.Vector3();
+    mk_pos.setFromMatrixPosition(marker_03.tag.object3D.matrixWorld);
+    let x = mk_pos.x * width ;
+    let y = mk_pos.y * -height;
+    let z = mk_pos.z * width/2;
+    let mk_rot = marker_03.tag.object3D.rotation;
+
+    translate(x, y, z);
+    rotateX(-mk_rot.x);
+    rotateY(mk_rot.y);
+    rotateZ(-mk_rot.z);
+    rotateX(PI/2);
+
+    texture(c_5);
+    noStroke();
+    rect(-360, -410, 720, 820);
+
+    translate(0, 0, 50);
+    texture(c_4);
+    noStroke();
+    rect(-360, -410, 720, 820);
+
+    translate(0, 0, 50);
+    texture(c_3);
+    noStroke();
+    rect(-360, -410, 720, 820);
+
+    translate(0, 0, 50);
+    texture(c_2);
+    noStroke();
+    rect(-360, -410, 720, 820);
+
+    translate(0, 0, 50);
+    texture(c_1);
+    noStroke();
+    rect(-360, -410, 720, 820);
+
+  }
+
+
+    if(marker_04.isVisible()==true){
+    let mk_pos = new THREE.Vector3();
+    mk_pos.setFromMatrixPosition(marker_04.tag.object3D.matrixWorld);
+    let x = mk_pos.x * width ;
+    let y = mk_pos.y * -height;
+    let z = mk_pos.z * width/2;
+    let mk_rot = marker_04.tag.object3D.rotation;
 
     translate(x, y, z);
     rotateX(-mk_rot.x);
@@ -291,6 +416,34 @@ function vidLoad() {
   a_12.loop();
   a_12.volume(0);
   a_12.hide();
+
+  b_2.loop();
+  b_2.volume(0);
+  b_2.hide();
+
+  b_3.loop();
+  b_3.volume(0);
+  b_3.hide();
+
+  b_4.loop();
+  b_4.volume(0);
+  b_4.hide();
+
+  c_2.loop();
+  c_2.volume(0);
+  c_2.hide();
+
+  c_3.loop();
+  c_3.volume(0);
+  c_3.hide();
+
+  c_4.loop();
+  c_4.volume(0);
+  c_4.hide();
+
+  c_5.loop();
+  c_5.volume(0);
+  c_5.hide();
 
   d_2.loop();
   d_2.volume(0);
