8d02587a50435855f5364494db176e44e44f41be
957064621 <957064621@qq.com>
2022-11-04 10:59:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,6 +27,8 @@ let cube = new Box({
 
 function setup() {
   // create our world (this also creates a p5 canvas for us)
+
+  lock();
   
   createCanvas(windowWidth, windowHeight, WEBGL);
   read = true;
@@ -494,4 +496,29 @@ function vidLoad() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+// (A) LOCK SCREEN ORIENTATION
+function lock (orientation) {
+  // (A1) GO INTO FULL SCREEN FIRST
+  let de = document.documentElement;
+  if (de.requestFullscreen) { de.requestFullscreen(); }
+  else if (de.mozRequestFullScreen) { de.mozRequestFullScreen(); }
+  else if (de.webkitRequestFullscreen) { de.webkitRequestFullscreen(); }
+  else if (de.msRequestFullscreen) { de.msRequestFullscreen(); }
+
+  // (A2) THEN LOCK ORIENTATION
+  screen.orientation.lock(orientation);
+}
+
+// (B) UNLOCK SCREEN ORIENTATION
+function unlock () {
+  // (B1) UNLOCK FIRST
+  screen.orientation.unlock();
+
+  // (B2) THEN EXIT FULL SCREEN
+  if (document.exitFullscreen) { document.exitFullscreen(); }
+  else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); }
+  else if (document.mozCancelFullScreen) { document.mozCancelFullScreen(); }
+  else if (document.msExitFullscreen) { document.msExitFullscreen(); }
 }
